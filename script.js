9da342324368e89eceb15bffe4ea7be16ce525e2
Kiara Correa Acosta <kcorrea@aclanyc.com>
2017-07-07 19:21:44 +0000

    cookies are great

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+$("#change").click(function() {
+    $("div").css("background-color","aqua");
+});
 
+$("#Picture").click(function(){
+for(var count = 0; count < 10 ; count++){
+    $("body").append('<img src="https://sistersoliloquies.files.wordpress.com/2014/06/img_7054.jpg">');
+    }
+});
 
 });
